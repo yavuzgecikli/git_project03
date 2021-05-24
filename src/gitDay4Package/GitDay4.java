f1ca2828b21f5eb43b9ab69f05afe1cd102380f3
Yavuz <yavuzgecikli@hotmail.com>
2021-05-24 10:31:48 -0400

    local branch first commit

--- src/gitDay4Package/GitDay4.java
+++ src/gitDay4Package/GitDay4.java
@@ -1,5 +1,10 @@
 package gitDay4Package;
 
 public class GitDay4 {
-
+	public static void main(String[] args) {
+		
+		System.out.println("local first commit main branch");
+		System.out.println("local branch first commit");
+		
+	}
 }
